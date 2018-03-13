a0da4b8e8a4628c826e5783aead8d4568ab3f1d3
willbrom <alisajid552@yahoo.com>
2018-03-13 19:30:56 +0300

    Add: FirebaseMessagingService
    To get push notifications

---
+++ app/src/main/java/com/wilbrom/mychatapp/sync/MyFirebaseMessagingService.java
@@ -0,0 +1,7 @@
+package com.wilbrom.mychatapp.sync;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+}
