3fa691e2dabba1ac3ee9781491920dd1bee0be43
willbrom <alisajid552@yahoo.com>
2018-03-14 09:20:57 +0300

    Add: Volley listeners

--- app/src/main/java/com/wilbrom/mychatapp/utils/NetworkUtils.java
+++ app/src/main/java/com/wilbrom/mychatapp/utils/NetworkUtils.java
@@ -14,35 +14,48 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class NetworkUtils {
-    private static RequestQueue requestQueue;
 
-    public static void sendHttpsResponse(Context context, String url) {
-        if (requestQueue == null)
-            requestQueue = Volley.newRequestQueue(context);
+    public interface OnTransactionCompletionListener {
+        void onSuccess(String response);
+        void onFailuer(String error);
+    }
+
+    private static OnTransactionCompletionListener mListener;
+
+    private static RequestQueue mRequestQueue;
+
+    public static void sendHttpsResponse(Context context, String url, final String title, final String message) {
+        mListener = (OnTransactionCompletionListener) context;
+
+        if (mRequestQueue == null)
+            mRequestQueue = Volley.newRequestQueue(context);
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
 
             @Override
             public void onResponse(String response) {
-
+                if (mListener != null)
+                    mListener.onSuccess(response);
             }
         }, new Response.ErrorListener() {
 
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                if (mListener != null)
+                    mListener.onFailuer(error.toString());
             }
         })
         {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
-                params.put("", "");
-                params.put("", "");
-                params.put("", "");
+                params.put("title", title);
+                params.put("message", message);
 
                 return params;
             }
         };
+
+        mRequestQueue.add(stringRequest);
     }
 }
